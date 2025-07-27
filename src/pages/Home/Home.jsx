bb1b71db01f809ab95adfd29b02cac562a7a8685
Vasu Bhalodiya <vasubhalodiya30@gmail.com>
2025-07-27 14:05:47 +0530

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,79 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { collection, query, where, getDocs } from 'firebase/firestore';
-// import { db, auth } from '../../firebase';
-// import './Home.css';
-// import Vaults from '../../components/Vaults/Vaults';
-// import VaultsData from '../../components/VaultsData/VaultsData';
-
-// const Home = ({ searchQuery }) => {
-//   const [vaultsLoaded, setVaultsLoaded] = useState(false);
-//   const [vaultsDataLoaded, setVaultsDataLoaded] = useState(false);
-//   const [hasData, setHasData] = useState(null);
-
-//   const isFullyLoaded = vaultsLoaded && vaultsDataLoaded && hasData !== null;
-
-//   useEffect(() => {
-//     const checkData = async () => {
-//       if (!auth.currentUser) return setHasData(false);
-      
-//       try {
-//         const usersQuery = query(collection(db, 'users'), where('uid', '==', auth.currentUser.uid));
-//         const userDocs = await getDocs(usersQuery);
-        
-//         if (userDocs.empty) return setHasData(false);
-        
-//         const vaultsQuery = query(collection(db, 'users', userDocs.docs[0].id, 'vaults'));
-//         const vaultsDocs = await getDocs(vaultsQuery);
-        
-//         setHasData(!vaultsDocs.empty);
-//       } catch (error) {
-//         setHasData(false);
-//       }
-//     };
-    
-//     checkData();
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="home">
-//         {!isFullyLoaded ? (
-//           <div className="home-loader-container">
-//             <div className="loader"></div>
-//           </div>
-//         ) : hasData ? (
-//           <div className="home-cnt master-cnt">
-//             <Vaults searchQuery={searchQuery} />
-//             <VaultsData />
-//           </div>
-//         ) : (
-//           <div className="home-no-data">
-//             <div className="no-data-message">
-//               <h3>No Data Found</h3>
-//               <p>You haven't created any vaults yet. Start by creating your first vault!</p>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-      
-//       {/* Hidden components for loading */}
-//       <div style={{ display: 'none' }}>
-//         <Vaults searchQuery={searchQuery} onLoaded={() => setVaultsLoaded(true)} />
-//         <VaultsData onLoaded={() => setVaultsDataLoaded(true)} />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Home;
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db, auth } from '../../firebase';
@@ -134,8 +58,10 @@ const Home = ({ searchQuery, refreshTrigger }) => {
         ) : (
           <div className="home-no-data">
             <div className="no-data-message">
-              <h3>No Data Found</h3>
-              <p>You haven't created any vaults yet. Start by creating your first vault!</p>
+              {/* <img src={images.failure} alt="failure" className='failure-img' /> */}
+              <i class="fa-light fa-face-frown-slight"></i>
+              <h3 className='failure-txt'>No Data Found</h3>
+              <p className='failure-subtxt'>You haven't created any vaults yet. Start by creating your first vault!</p>
             </div>
           </div>
         )}
--- src/utils/Images.jsx
+++ src/utils/Images.jsx
@@ -3,7 +3,6 @@ import premium from '../assets/images/premium-star.svg';
 import my_avtar from '../assets/images/my-avtar.jpg';
 import click_to_copy from '../assets/images/click-to-copy.svg';
 import password_gif from '../assets/images/password.gif';
-import failure from '../assets/images/failure.png';
 
 const images = {
     logo,
@@ -11,7 +10,6 @@ const images = {
     my_avtar,
     click_to_copy,
     password_gif,
-    failure
 };
 
 export default images;
